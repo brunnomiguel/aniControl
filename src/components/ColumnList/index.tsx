457df8abb18ab0995ad80b7c38312e27eee16e76
William Baierle <w.baierle@gmail.com>
2022-08-31 22:22:16 -0300

    feat: dashboard card component structurized

--- src/components/ColumnList/index.tsx
+++ src/components/ColumnList/index.tsx
@@ -1,27 +1,20 @@
-import { Flex, Text, VStack } from '@chakra-ui/react';
+import { Flex, Text, VStack } from "@chakra-ui/react";
 
-
-interface ICard {
-    img: string;
-    title: string;
-    sinopsis: string;
-    rating: number;
-    year: string;
-    genres: string[];
-}
+interface ICard {}
 
 interface IColumnList {
-    cardList?: ICard[];
-} 
+  cardList?: ICard[];
+}
 
-export const ColumnList = ({cardList}: IColumnList) => {
+export const ColumnList = ({ cardList }: IColumnList) => {
   return (
     <Flex>
-        <VStack>
-            { cardList && cardList.map((card) => {
-                return <Text>Test</Text>
-            })}
-        </VStack>
+      <VStack>
+        {cardList &&
+          cardList.map((card) => {
+            return <Text>Test</Text>;
+          })}
+      </VStack>
     </Flex>
-  )
-}
+  );
+};
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import { Header } from "../../components/Header";
+
+export const Dashboard = () => {
+  return (
+    <Box w="100vw" h="100vh" bgColor="#21212D">
+      <Header />
+    </Box>
+  );
+};
