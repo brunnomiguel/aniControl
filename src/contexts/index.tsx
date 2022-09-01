46066f7786c3dad5c38e7396e0df120090329393
Brunno Miguel <miguel.brunno06@gmail.com>
2022-08-31 21:56:43 -0300

    Feat: created index provider in the application

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,1 +1,16 @@
-export {};
+import { ReactNode } from "react";
+
+import { AuthProvider } from "./Auth";
+import { ChakraProvider } from "@chakra-ui/react";
+
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+  return (
+    <AuthProvider>
+      <ChakraProvider>{children}</ChakraProvider>
+    </AuthProvider>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import { App } from "./App";
+import { AppProvider } from "./contexts";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
