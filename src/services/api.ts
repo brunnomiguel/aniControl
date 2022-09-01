91ed803b77cd7efd17e8cd75a4c798e418132d2c
Brunno Miguel <miguel.brunno06@gmail.com>
2022-08-31 21:37:42 -0300

    feat: auth context with sign in and sign out functions

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,9 @@
-export {};
+import axios from "axios";
+
+export const jsonApi = axios.create({
+  baseURL: "https://json-server-anicontrol.herokuapp.com/",
+});
+
+export const jikanApi = axios.create({
+  baseURL: "https://api.jikan.moe/v4/",
+});
