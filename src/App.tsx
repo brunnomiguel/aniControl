06d10efb46a2c9dc3e3768ffc4a50d067c6dd9cb
Eric James <35430109+Ericjcf@users.noreply.github.com>
2022-09-02 18:24:41 -0300

    feat: inseridos os Input usando o component, e hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { Dashboard } from "./pages/Dashboard";
 import { SignUp } from "./pages/SignUp";
+import { SignIn } from "./pages/SignIn";
 
 export const App = () => {
 	return (
 		<ChakraProvider>
-			<SignUp />
+			<SignIn />
+			{/* <SignUp/> */}
 		</ChakraProvider>
 	);
 };
