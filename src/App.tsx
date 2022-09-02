7813a50c3cb12a5b80bddbd8bf4e73d63656680f
Brunno Miguel <miguel.brunno06@gmail.com>
2022-09-02 15:50:56 -0300

    feat: index Routes created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { Browse } from "./pages/Browse";
-import { Dashboard } from "./pages/Dashboard";
-import { SignUp } from "./pages/SignUp";
+import { MainRoutes } from "./routes";
 
 export const App = () => {
-  return <Browse />;
+  return <MainRoutes />;
 };
