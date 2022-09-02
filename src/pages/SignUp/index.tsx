78d1b422303f48548da65036e0b97dd8c4abc3d3
DevBraz <engcivilbraz@hotmail.com>
2022-09-01 21:37:06 -0300

    feat: att mobile version

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,16 +1,35 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, useBreakpointValue } from "@chakra-ui/react";
 import { BackgroundAnimation } from "../../components/BackgroundAnimation";
 import { BackgroundImg } from "./BackgroundImg";
 import { SignUpForm } from "./SignUpForm";
 
 export const SignUp = () => {
+	const isWideVersion = useBreakpointValue({
+		base: false,
+		md: true,
+	});
 	return (
-		<Box>
-			<BackgroundAnimation />
-			<Flex position="fixed" top="0" left="0" flexDir="row" h="100vh" w="100vw">
-				<SignUpForm />
-				<BackgroundImg />
-			</Flex>
-		</Box>
+		<>
+			{isWideVersion ? (
+				<Box>
+					<BackgroundAnimation />
+					<Flex
+						position="fixed"
+						top="0"
+						left="0"
+						flexDir="row"
+						h="100vh"
+						w="100vw"
+					>
+						<SignUpForm />
+						<BackgroundImg />
+					</Flex>
+				</Box>
+			) : (
+				<Flex bg="#21212D" flexDir="row" justifyContent="center">
+					<SignUpForm />
+				</Flex>
+			)}
+		</>
 	);
 };
