fd581ee9acb525e7efdcaa9bc56b37a467a9f64d
Brunno Miguel <miguel.brunno06@gmail.com>
2022-09-05 18:56:11 -0300

    fix: duplicated imports in the routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import { Browse } from "../pages/Browse";
 import { Dashboard } from "../pages/Dashboard";
 import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
-import { SignIn } from "../pages/SignIn";
 
 import ProtectedRoute from "./protectedRoute";
 
@@ -12,8 +11,6 @@ export const MainRoutes = () => {
     <Routes>
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/" element={<SignIn />} />
-      <Route path="/signin" element={<SignIn />} />
       <Route element={<ProtectedRoute />}>
         <Route path="/dashboard" element={<Dashboard />} />
       </Route>
