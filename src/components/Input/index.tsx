a95c031cee3afa4c575667e48b73f4591376461e
DevBraz <engcivilbraz@hotmail.com>
2022-09-06 20:41:36 -0300

    feat: added show and hide password and confirm password state

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,70 +1,67 @@
 import {
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  Input as ChakraInput,
-  InputProps as ChakraInputProps,
-  InputLeftElement,
-  InputGroup,
-  border,
+	FormControl,
+	FormLabel,
+	FormErrorMessage,
+	Input as ChakraInput,
+	InputProps as ChakraInputProps,
+	InputLeftElement,
+	InputGroup,
+	border,
 } from "@chakra-ui/react";
 
 import {
-  useState,
-  useEffect,
-  useCallback,
-  useRef,
-  ForwardRefRenderFunction,
-  forwardRef,
+	useState,
+	useEffect,
+	useCallback,
+	useRef,
+	ForwardRefRenderFunction,
+	forwardRef,
 } from "react";
 import { FieldError } from "react-hook-form";
 import { IconType } from "react-icons";
 import { FaExclamation } from "react-icons/fa";
 
 interface InputProps extends ChakraInputProps {
-  label?: string;
-  name: string;
-  //   name apenas, pois pode ser email, senha ou uma busca generica
-  error?: FieldError | null;
-  icon?: IconType;
+	label?: string;
+	name: string;
+	//   name apenas, pois pode ser email, senha ou uma busca generica
+	error?: FieldError | null;
+	icon?: IconType;
 }
 
-export const InputAni: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({
-  name,
-  error = null,
-  icon: Icon,
-  label,
-  ...rest
-}, ref) => {
-  // label deve ser opcional, pois o input tb será usado na barra de busca
-  return (
-    <FormControl mt="5">
-      {label && <FormLabel>{label}</FormLabel>}
+export const InputAni: ForwardRefRenderFunction<
+	HTMLInputElement,
+	InputProps
+> = ({ name, error = null, icon: Icon, label, ...rest }, ref) => {
+	// label deve ser opcional, pois o input tb será usado na barra de busca
+	return (
+		<FormControl mt="5">
+			{label && <FormLabel>{label}</FormLabel>}
 
-      <InputGroup flexDirection="column">
-        {Icon && (
-          <InputLeftElement mr="2" mt="1.5" color="grey.900">
-            <Icon />
-          </InputLeftElement>
-        )}
-        {/* ver erro com o rest do input */}
-        <ChakraInput
-          name={name}
-          {...rest}
-          ref={ref}
-          bg="grey.0"
-          borderRadius="2"
-          variant="outline"
-          color="grey.900"
-          //   _hover={{ border = "solid 3px blue.50" }} rever este hover depois
-          size="4x1"
-          w='100%'
-          h="50px"
-        ></ChakraInput>
-        {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
-      </InputGroup>
-    </FormControl>
-  );
+			<InputGroup flexDirection="column">
+				{Icon && (
+					<InputLeftElement mr="2" mt="1.5" color="red.600">
+						<Icon />
+					</InputLeftElement>
+				)}
+				{/* ver erro com o rest do input */}
+				<ChakraInput
+					name={name}
+					{...rest}
+					ref={ref}
+					bg="grey.0"
+					borderRadius="2"
+					variant="outline"
+					color="grey.900"
+					//   _hover={{ border = "solid 3px blue.50" }} rever este hover depois
+					size="4x1"
+					w="100%"
+					h="50px"
+				></ChakraInput>
+				{!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+			</InputGroup>
+		</FormControl>
+	);
 };
 
 export const Input = forwardRef(InputAni);
