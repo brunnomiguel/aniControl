be5fdbbafadd7bb6532a84e17285786cedd28b79
Brunno Miguel <miguel.brunno06@gmail.com>
2022-09-06 23:35:03 -0300

    fix: header positions and alignments of items. adjustment in the component Input and sign in page.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,60 +1,66 @@
-import { Box, Button, Flex, Image, Input, theme } from "@chakra-ui/react";
-import { useState } from "react";
+import { Box, Center, Flex, Image } from "@chakra-ui/react";
+
+import { Input } from "../Input";
 import { FaSearch } from "react-icons/fa";
+
 import Logo from "../../assets/logo-dash.svg";
+import { useForm } from "react-hook-form";
+import { useFullAnimes } from "../../contexts/FullAnimes";
+
+interface SearchData {
+  animeName: string;
+}
 
 export const Header = () => {
-  const [openSearch, setOpenSearch] = useState(true);
+  const { handleSubmit, register } = useForm<SearchData>();
+  const { searchAnime } = useFullAnimes();
 
-  const handleOpenSearch = () => {
-    setOpenSearch(!openSearch);
+  const handleSearchAnime = ({ animeName }: SearchData) => {
+    searchAnime(animeName);
   };
 
   return (
     <Flex
       as="header"
-      bgColor="red"
       w="100vw"
       h="7vh"
-      align="center"
+      alignItems="center"
       justify="space-between"
       backgroundColor="grey.600"
       borderBottom={"0.2rem solid"}
       borderBottomColor={"blue.50"}
     >
-      <Image h="70%" w={["35%", "35%", "10%", "10%"]} src={Logo} />
-      <>
-        {openSearch ? (
-          <Flex
-            align="center"
-            justify="center"
-            w={["50%", "35%", "25%", "25%"]}
-            h="70%"
-            bgColor="grey.0"
-            borderRadius="8px"
-            border="0.1rem solid"
-            borderColor="grey.900"
-            mr="2vw"
-          >
-            <Input w="85%" h="100%" outline="none" variant="unstyled" ml="1%" />
-            <Button
-              w="10%"
-              h="90%"
-              bgColor="red.600"
-              _hover={{ bg: "pink.800" }}
-              _active={{ bg: "red.600" }}
-              onClick={handleOpenSearch}
-              mr="2%"
-            >
-              <FaSearch color="grey.0" />
-            </Button>
-          </Flex>
-        ) : (
-          <Box mr="3.5vw">
-            <FaSearch color="grey.0" onClick={handleOpenSearch} />
-          </Box>
-        )}
-      </>
+      <Image
+        h="70%"
+        w={["35%", "35%", "10%", "10%"]}
+        src={Logo}
+        ml={["2", "4", "4"]}
+      />
+      <Box
+        as="form"
+        onSubmit={handleSubmit(handleSearchAnime)}
+        position="relative"
+        mr={["2", "4", "6"]}
+        w={["auto", "auto", "50%", "27%"]}
+      >
+        <Input {...register("animeName")} />
+        <Center
+          as="button"
+          w="40px"
+          h="40px"
+          bgColor="red.600"
+          borderRadius="6"
+          _hover={{ bg: "pink.800" }}
+          _active={{ bg: "red.600" }}
+          position="absolute"
+          top="1"
+          right="2"
+          type="submit"
+          zIndex="1"
+        >
+          <FaSearch color="grey.0" />
+        </Center>
+      </Box>
     </Flex>
   );
 };
