97cfa3d95203812fa76271e642ceee42de315984
Brunno Miguel <miguel.brunno06@gmail.com>
2022-09-09 22:31:12 -0300

    refact: viewing video for the large screen

--- src/pages/Browse/index.tsx
+++ src/pages/Browse/index.tsx
@@ -1,4 +1,11 @@
-import { Center, filter, Flex, Grid, Text } from "@chakra-ui/react";
+import {
+  Center,
+  filter,
+  Flex,
+  Grid,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { BiFirstPage, BiLastPage } from "react-icons/bi";
 import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
@@ -10,60 +17,65 @@ import { BrowseVideos } from "../../components/BrowseVideo";
 import { arrayVideo } from "../../components/BrowseVideo/videos";
 
 export const Browse = () => {
-	const { allAnimes, getAllAnimes } = useFullAnimes();
-	const [page, setPage] = useState(1);
+  const { allAnimes, getAllAnimes } = useFullAnimes();
+  const [page, setPage] = useState(1);
 
-	const previusPage = () => {
-		if (page > 1) {
-			setPage(page - 1);
-		}
-	};
+  const previusPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
 
-	useEffect(() => {
-		getAllAnimes(page);
-	}, [page]);
+  useEffect(() => {
+    getAllAnimes(page);
+  }, [page]);
 
-	return (
-		<>
-			<Header />
-			<BrowseVideos arrayVideo={arrayVideo} />
-			<Flex
-				flexDirection="column"
-				bgColor="grey.700"
-				padding={["2", "2", "10"]}
-				w="100vw"
-				minH="100vh"
-			>
-				<Flex
-					w="100%"
-					justifyContent="space-between"
-					direction={["column-reverse", "column-reverse", "row", "row"]}
-				>
-					<Grid
-						w="100%"
-						templateColumns="repeat(auto-fill, minmax(210px, 1fr))"
-						// justifyItems="center"
-						gap="10"
-					>
-						{allAnimes?.map((anime) => {
-							return <ImageCard key={anime.mal_id} anime={anime} />;
-						})}
-					</Grid>
-					<Grid h="90vh" w={["100%", "100%", "500px"]}>
-						<TopAnimes />
-						<SeasonsAnimes />
-					</Grid>
-				</Flex>
-				<Flex>
-					<Center onClick={previusPage}>
-						<BiFirstPage />
-					</Center>
-					<Text>{page}</Text>
-					<Center onClick={() => setPage(page + 1)}>
-						<BiLastPage />
-					</Center>
-				</Flex>
-			</Flex>
-		</>
-	);
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    xl: true,
+  });
+
+  return (
+    <>
+      <Header />
+      {isWideVersion && <BrowseVideos arrayVideo={arrayVideo} />}
+      <Flex
+        flexDirection="column"
+        bgColor="grey.700"
+        padding={["2", "2", "10"]}
+        w="100vw"
+        minH="100vh"
+      >
+        <Flex
+          w="100%"
+          justifyContent="space-between"
+          direction={["column-reverse", "column-reverse", "row", "row"]}
+        >
+          <Grid
+            w="100%"
+            templateColumns="repeat(auto-fill, minmax(210px, 1fr))"
+            // justifyItems="center"
+            gap="10"
+          >
+            {allAnimes?.map((anime) => {
+              return <ImageCard key={anime.mal_id} anime={anime} />;
+            })}
+          </Grid>
+          <Grid h="90vh" w={["100%", "100%", "500px"]}>
+            <TopAnimes />
+            <SeasonsAnimes />
+          </Grid>
+        </Flex>
+        <Flex>
+          <Center onClick={previusPage}>
+            <BiFirstPage />
+          </Center>
+          <Text>{page}</Text>
+          <Center onClick={() => setPage(page + 1)}>
+            <BiLastPage />
+          </Center>
+        </Flex>
+      </Flex>
+    </>
+  );
 };
