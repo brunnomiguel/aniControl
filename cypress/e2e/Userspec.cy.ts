7142657ed87fd0b1b7bf1453abe2a566c27ee4c6
edson vinicius <edsonvin1801@gmail.com>
2022-09-21 19:36:25 -0300

    feat: test register

--- cypress/e2e/Userspec.cy.ts
+++ cypress/e2e/Userspec.cy.ts
@@ -1,5 +1,13 @@
-describe("empty spec", () => {
-  it("passes", () => {
+context("User path", () => {
+  it("Enters the landing page and tries to go to the register page", () => {
     cy.visit("http://localhost:3000");
+    cy.viewport(1440, 900);
+    cy.get(".css-6iyf1g").contains(/anicontrol/i);
   });
+
+  // it("Enters the register page and creates an user", () => {
+  //   cy.visit("http://localhost:3000/signin");
+  //   cy.viewport(1440, 900);
+  //   cy.get(".css-1i22j4g").click();
+  // });
 });
